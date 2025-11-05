fd0b65f16cc2d0a91a682e5d99a470dab8a8bf15
chengyu-ucsb <chengyu@ucsb.edu>
2025-11-04 22:39:33 -0800

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// firebase-config.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyCoq5nM17RTBXDZOlwWBsIm7x6es_VJii0",
+  authDomain: "diss-writing-tracker.firebaseapp.com",
+  databaseURL: "https://diss-writing-tracker-default-rtdb.firebaseio.com",
+  projectId: "diss-writing-tracker",
+  storageBucket: "diss-writing-tracker.firebasestorage.app",
+  messagingSenderId: "954028268463",
+  appId: "1:954028268463:web:xxxxxxxxxxxxxx"
+};
